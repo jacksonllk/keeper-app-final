93ca4c661787c4ace332300854292d5f0878b6ff
Jackson Ling <96425149+jacksonllk@users.noreply.github.com>
2023-03-23 22:49:53 +0800

    create Note object and store in Notes array

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState({ title: "", content: "" });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {...prevNote, [name]: value};
+    });
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    setNotes((prevNotes) => {
+      return [...prevNotes, note]
+    });
+    setNote({ title: "", content: "" })
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={note.title}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          onChange={handleChange}
+          value={note.content}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button>Add</button>
       </form>
     </div>
